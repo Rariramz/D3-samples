ca76fde6f5e6304309642bb3d523c7b7c5367dbb
Rariramz <mary4798713@gmail.com>
2022-03-19 02:33:14 +0300

    some refactoring and filter added

--- app.js
+++ app.js
@@ -10,74 +10,24 @@ Promise.all([
       throw err;
     }),
 ])
-  .then(([obj_0, obj_1]) => {
-    visualize("usa", obj_0.data, "USA Population");
-    for (let state of groupByKey(obj_1.data, "Slug State")) {
-      visualize(state.key, state.values, `${state.values[0].State} Population`);
-    }
+  .then(([wholePopulation, statesPopulation]) => {
+    visualize(wholePopulation);
+    const groupedData = groupByKey(statesPopulation.data, "Slug State");
+    addSelect(["usa"].concat(groupedData));
+    visualize(groupedData);
 
-    function visualize(elementId, data, title, parentSelector = "body") {
-      d3.select(parentSelector)
-        .append("figure")
-        .attr("id", elementId)
-        .append("figcapture")
-        .attr("class", "title")
-        .text(title);
-
-      d3.select(`#${elementId}`).append("div").attr("class", "diagram");
-
-      // ENTER
-      d3.select(`#${elementId}`)
-        .select("div.diagram")
-        .selectAll("div.item")
-        .data(data)
-        .enter()
-        .append("div")
-        .attr("class", "item")
-        .append("div")
-        .attr("class", "data")
-        .append("span");
-
-      // UPDATE
-      let scale = d3.scale
-        .linear()
-        .domain([0, getMaxYearsPopulation(data)])
-        .range([0, 250]);
-
-      d3.select(`#${elementId}`)
-        .select("div.diagram")
-        .selectAll("div.item")
-        .data(data)
-        .selectAll("div.data")
-        .style("width", (d) => {
-          return scale(d.Population) + "px";
+    function selectHandler() {
+      d3.select("body").selectAll("figure").classed("unselected", false);
+      let filterState = document.getElementById("select").value;
+      d3.select("body")
+        .selectAll("figure")
+        .data([wholePopulation].concat(groupedData))
+        .filter((d) => {
+          if (filterState && filterState !== "All")
+            return !(d.key == filterState);
+          else return false;
         })
-        .select("span")
-        .text((d) => d.Population.toLocaleString());
-
-      d3.select(`#${elementId}`)
-        .select("div.diagram")
-        .selectAll("div.item")
-        .data(data)
-        .append("div")
-        .attr("class", "year")
-        .text((d) => d.Year);
-
-      // EXIT
-      d3.select(`#${elementId}`)
-        .select("div.diagram")
-        .selectAll("div.item")
-        .data(data)
-        .exit()
-        .remove();
-
-      function getMaxYearsPopulation(years) {
-        let populations = [];
-        for (let year of years) {
-          populations.push(year.Population);
-        }
-        return d3.max(populations);
-      }
+        .classed("unselected", true);
     }
 
     function groupByKey(data, key) {
@@ -88,6 +38,87 @@ Promise.all([
         })
         .entries(data);
       return nest;
+    }
+
+    function addSelect(options) {
+      d3.select("select")
+        .on("change", () => selectHandler())
+        .selectAll("option")
+        .data(options)
+        .enter()
+        .append("option");
+      d3.select("select")
+        .selectAll("option")
+        .data(options)
+        .text((opt) => opt.key || "All");
+    }
+
+    function visualize(data, filterState = null, parent = "body") {
+      if (Array.isArray(data)) {
+        for (let state of data) {
+          visualizeOne(state.key, state.values, parent);
+        }
+      } else {
+        visualizeOne("usa", data.data, parent);
+      }
+
+      function visualizeOne(key, values, parent) {
+        // ENTER
+        d3.select(parent)
+          .append("figure")
+          .attr("id", key)
+          .append("figcapture")
+          .attr("class", "title")
+          .text(`${key.toUpperCase()} Population`);
+
+        d3.select(`#${key}`).append("div").attr("class", "diagram");
+
+        d3.select(`#${key}`)
+          .select("div.diagram")
+          .selectAll("div.item")
+          .data(values)
+          .enter()
+          .append("div")
+          .attr("class", "item")
+          .append("div")
+          .attr("class", "data")
+          .append("span");
+
+        // UPDATE
+        function getMaxYearsPopulation(years) {
+          let populations = [];
+          for (let year of years) {
+            populations.push(year.Population);
+          }
+          //console.log(d3.max(populations));
+          return d3.max(populations);
+        }
+        let scale = d3.scale
+          .linear()
+          .domain([0, getMaxYearsPopulation(values)])
+          .range([0, 250]);
+
+        d3.select(`#${key}`)
+          .select("div.diagram")
+          .selectAll("div.item")
+          .data(values)
+          .select("div.data")
+          .style("width", (d) => {
+            return scale(d.Population) + "px";
+          })
+          .select("span")
+          .text((d) => {
+            return d.Population.toLocaleString();
+          });
+
+        d3.select(`#${key}`)
+          .select("div.diagram")
+          .selectAll("div.item")
+          .data(values)
+          .append("div")
+          .attr("class", "year")
+          .text((d) => d.Year);
+      }
     }
   })
   .catch((err) => {
